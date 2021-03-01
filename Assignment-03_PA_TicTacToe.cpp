2494dc6bac22edc67136c4612fdb25d634911f0a
Cody Huang <wenhuang@umail.ucsb.edu>
2021-03-01 10:25:47 -0800

    Update Assignment-03_PA_TicTacToe.cpp
    added display and makeAMove

--- Assignment-03_PA_TicTacToe.cpp
+++ Assignment-03_PA_TicTacToe.cpp
@@ -3,8 +3,34 @@ using namespace std;
 
 bool isWon(char, char[][3]);
 bool isDraw(char[][3]);
-void displayBoard(char[][3]);
-void makeAMove(char[][3], char);
+
+void displayBoard(char board[3][3])
+{
+    for(int i = 0; i < 3; i++){
+		cout<<"\n-------------\n";
+		//cout << i <<"= i\n";
+		for(int j = 0; j < 3; j++){
+			//cout << j <<"= j\n";
+			cout <<"| "<<board[i][j]<<" ";
+		}
+		cout<<"|";
+	}
+	cout<<"\n-------------\n";
+}
+void makeAMove(char board[3][3], char player){
+	int row;
+	int col;
+	cout << "Enter a row (0, 1, 2) for player "<< player<<"   : ";
+	cin >>  row;
+	cout << "Enter a column (0, 1, 2) for player "<< player<<": ";
+	cin >> col;
+	if(board[row][col]==' '){
+		board[row][col]=player;
+	} else{
+		cout <<"This cell is already occupied. Try a different cell\n";
+		makeAMove(board,player);
+	}
+}
 
 int main() {
 	//
@@ -12,7 +38,11 @@ int main() {
 	//
 	char board[3][3] = { { ' ', ' ', ' ' },{ ' ', ' ', ' ' },{ ' ', ' ', ' ' } };
 	displayBoard(board);
-
+	while(true){
+		makeAMove(board, 'X');
+		displayBoard(board);
+	}
+	/*
 	while (true) {
 		// The first player makes a move
 		makeAMove(board, 'X');
@@ -38,7 +68,7 @@ int main() {
 			cout << "No winner" << endl;
 			exit(0);
 		}
-	}
+	}*/
 
 	return 0;
 } 
